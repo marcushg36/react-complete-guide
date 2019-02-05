cce19d43e8e46d25ce91c06e4fd0a713c24f4ab7
Marcus <madhops.dunks@gmail.com>
2019-02-04 20:15:56 -0700

    did more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 	state = {
@@ -47,48 +48,43 @@ class App extends Component {
 	}
 
 	render() {
-		const style = {
-			backgroundColor: 'green',
-			color: 'white',
-			font: 'inherit',
-			border: '1px solid blue',
-			padding: '8px',
-			cursor: 'pointer'
-		};
 
 		let persons = null;
+		let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => {
-						return <Person
-							click={this.deletePersonHandler.bind(this, index)}
-							name={person.name}
-							age={person.age}
-							key={person.id}
-							changed={(event) => this.nameChangedHandler(event, person.id)} />
+						return <ErrorBoundary key={person.id}>
+							<Person
+								click={this.deletePersonHandler.bind(this, index)}
+								name={person.name}
+								age={person.age}
+								key={person.id}
+								changed={(event) => this.nameChangedHandler(event, person.id)} />
+							</ErrorBoundary>
 					})}
 				</div>
 			);
 
-			style.backgroundColor = 'red';
+			btnClass = styles.Red;
 		}
 
 		const classes = [];
 		if (this.state.persons.length <= 2) {
-			classes.push('red'); // classes = ['red']
+			classes.push(classes.red); // classes = ['red']
 		}
 		if (this.state.persons.length <= 1) {
-			classes.push('bold'); // classes = ['red', 'bold']
+			classes.push(classes.bold); // classes = ['red', 'bold']
 		}
 
 		return (
-			  <div className="App">
+			  <div className={styles.App}>
 			    <h1>Hi, I'm a React App</h1>
 					<p className={classes.join(' ')}>This is really working!</p>
 					<button
-						style={style}
+						className={btnClass}
 						onClick={this.togglePersonsHandler}>Toggle Persons</button>
 					{persons}
 			  </div>
